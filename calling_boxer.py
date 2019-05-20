91ed054d1d223280d7f994be9505d469dafeb978
dstrohmaier <davidstrohmaier92@gmail.com>
2019-05-20 10:14:46 +0100

    added first data and started to write calling_boxer

---
+++ calling_boxer.py
@@ -0,0 +1,55 @@
+from subprocess import Popen, PIPE
+
+import nltk
+
+def parse(tokenized):
+    parser_options = ['--models',
+                      'parser/models/boxer'] # depends on installation
+    
+    parser = ['parser/bin/candc'] # depends on installation
+
+    process = subprocess.Popen(parser + parser_options,
+                               shell=False,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = process.communicate(tokenized)
+    
+    if err:
+        # ignores info C&C writes to stderr
+        if not err.startswith('#'):
+            log.error('Parser error: {0}'.format(err))
+    parsed = out.decode('utf-8').encode("utf-8")
+    return parsed
+
+
+def box(parsed):
+    boxer = ['parser/bin/boxer'] # depends on installation
+    
+    boxer_options = ['--stdin',
+                     '--instantiate', 'true',
+                     '--resolve', 'true',
+                     '--semantics', 'drs',
+                     '--format', 'xml']
+
+
+    process = subprocess.Popen(boxer + boxer_options,
+                               shell=False,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = process.communicate(parsed)
+
+    if err:
+        # we ignore an error Boxer throws wrongly
+        if not "No source location" in err:
+            log.error('Boxer error: {0}'.format(err))
+    boxed = out.decode('utf-8').encode("utf-8")
+
+    return boxed
+
+
+def box_sent(sent):
+    tokenized = nltk.word_tokenize(sent)
+    parsed = parse(tokenized)
+    boxed = box(parsed)
