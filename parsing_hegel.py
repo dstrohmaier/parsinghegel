ab1a6c299d10a7c46d7d1e62c56bec7352614f62
dstrohmaier <davidstrohmaier92@gmail.com>
2019-05-22 17:12:08 +0100

    small bug with open

--- parsing_hegel.py
+++ parsing_hegel.py
@@ -8,7 +8,7 @@ DATA_DIR = BASE_DIR.joinpath("data")
 
 def parse_intro():
     intro_path = DATA_DIR.joinpath("first_sentences.txt")
-    with open(intro_path) as in_file:
+    with open(intro_path, "r") as in_file:
         text = in_file.read() # It is just one line
 
     intro_parsed = box_sent(text)
