4c5e4d328d30491a0cfdef902303c4c2c3b9f7dc
dstrohmaier <davidstrohmaier92@gmail.com>
2019-05-22 17:22:45 +0100

    enabled for python <3.6

--- parsing_hegel.py
+++ parsing_hegel.py
@@ -8,7 +8,7 @@ DATA_DIR = BASE_DIR.joinpath("data")
 
 def parse_intro():
     intro_path = DATA_DIR.joinpath("first_sentences_intro.txt")
-    with open(intro_path, "r") as in_file:
+    with intro_path.open(mode="r") as in_file: # accommodates versions <3.6
         text = in_file.read() # It is just one line
 
     intro_parsed = box_sent(text)
