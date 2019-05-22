e7074f70184069a120ab4acadc2575ccb83e3ef2
dstrohmaier <davidstrohmaier92@gmail.com>
2019-05-22 16:42:28 +0100

    added a small test because of problems with invoking Boxer

---
+++ test_boxer.py
@@ -0,0 +1,12 @@
+"""
+A small test to establish whether the parser works with tested input.
+"""
+
+from calling_boxer import parse, box
+
+
+if __name__ == "__main__":
+    parsed = parse("The man in the car saw the dog run across the street .\n He gave her dog biscuits .")
+    result = box(parsed)
+
+    return result
