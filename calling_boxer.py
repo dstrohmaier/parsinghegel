8f70041c375551542fcd47e3417ccefdd1f3eb11
dstrohmaier <davidstrohmaier92@gmail.com>
2019-05-22 16:46:51 +0100

    added a print to calling_boxer, still debugging

--- calling_boxer.py
+++ calling_boxer.py
@@ -55,6 +55,7 @@ def box(parsed):
 def box_sent(sent):
     sent += sent + "\n\n<EOF>" # trying a dirty bug fix for Boxer here
     tokenized = " ".join(nltk.word_tokenize(sent))
+    print(tokenized)
     parsed = parse(tokenized)
     print(parsed)
     boxed = box(parsed)
