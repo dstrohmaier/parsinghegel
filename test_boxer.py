e9f500c6d5caf853a5df8805544d05c9fdaf51d0
dstrohmaier <davidstrohmaier92@gmail.com>
2019-05-22 16:45:28 +0100

    added a print to small test

--- test_boxer.py
+++ test_boxer.py
@@ -7,6 +7,7 @@ from calling_boxer import parse, box
 
 if __name__ == "__main__":
     parsed = parse("The man in the car saw the dog run across the street .\n He gave her dog biscuits .")
+    print(parsed)
     result = box(parsed)
 
     print(result)
